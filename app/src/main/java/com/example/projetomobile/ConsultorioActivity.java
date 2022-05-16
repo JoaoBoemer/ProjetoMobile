58123b213b8b491304d99875a2255cb3e31ef751
Jgcbitj <67125125+JoaoBoemer@users.noreply.github.com>
2022-05-16 00:51:10 -0300

    Botao voltar no consultorio

--- app/src/main/java/com/example/projetomobile/ConsultorioActivity.java
+++ app/src/main/java/com/example/projetomobile/ConsultorioActivity.java
@@ -3,6 +3,8 @@ package com.example.projetomobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConsultorioActivity extends AppCompatActivity {
 
@@ -10,8 +12,17 @@ public class ConsultorioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultorio);
-
+        configureBackButton();
     }
 
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.buttonVoltar3);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/projetomobile/MenuActivity.java
+++ app/src/main/java/com/example/projetomobile/MenuActivity.java
@@ -30,7 +30,7 @@ public class MenuActivity extends AppCompatActivity {
         RegisterCompanyButton();
         consultorioButton();
 
-        
+
 
     }
 
